489e1fa364ec3f1a92fda94dfdd4caad12c1ac1a
leslie <daysofbeingwild@126.com>
2016-07-21 18:23:27 +0800

    commdemo

--- handler/login_request.go
+++ handler/login_request.go
@@ -2,7 +2,9 @@ package handler
 
 import (
 	"fmt"
+	"regexp"
 	"withjewel/jewel"
+	"withjewel/model"
 )
 
 /*LoginRequestHandler 处理登录请求 */
@@ -18,13 +20,43 @@ func (this *LoginRequestHandler) Get() {
 
 /*Post 验证登录*/
 func (this *LoginRequestHandler) Post() {
+	UsernameRegex := regexp.MustCompile(`[A-Za-z0-9]{4}`)
+	EmailRegex := regexp.MustCompile(`\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*`)
+
 	username := this.Ctx.Input.FormValue("username")
 	password := this.Ctx.Input.FormValue("password")
 
-	model := make(map[string]string)
-	model["loginStatus"] = "Failed"
-	model["loginMessage"] = "Error password"
-	
-	fmt.Printf("%s请求登录, 密码%s\n", username, password)
-	jewel.RenderTplFile(this.Ctx.Output, "views/login.html", model)
+	datamodel := make(map[string]string)
+
+	var pass = false
+
+	if username == "" {
+		datamodel["loginStatus"] = "失败"
+		datamodel["loginMessage"] = "你小子肯定是通过非法客户端连接的。"
+	} else {
+		if len(UsernameRegex.FindString(username)) == len(username) {
+			pass = model.Verify(username, password)
+			if !pass {
+				datamodel["loginStatus"] = "失败"
+				datamodel["loginMessage"] = "错误的用户名或密码，或许您需要填写一个电子邮件来加入？"
+			}
+		} else if len(EmailRegex.FindString(username)) == len(username) {
+			pass = model.VerifyEmail(username, password)
+			if !pass {
+				datamodel["loginStatus"] = "成功"
+				datamodel["loginMessage"] = "我们已经向" + username + "发送了一封确认电子邮件。"
+			}
+		} else {
+			datamodel["loginStatus"] = "失败"
+			datamodel["loginMessage"] = "你小子肯定是通过非法客户端连接的。"
+		}
+	}
+
+	if pass {
+		datamodel["loginStatus"] = "成功"
+		datamodel["loginMessage"] = "欢迎您"
+	}
+
+	//fmt.Printf("%s请求登录, 密码%s\n", username, password)
+	jewel.RenderTplFile(this.Ctx.Output, "views/login.html", datamodel)
 }
---
+++ model/user.go
@@ -0,0 +1,71 @@
+package model
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type User struct {
+	id                 int
+	username, password string
+}
+
+func Verify(username string, password string) bool {
+	db, err := sql.Open("mysql", "leslie:19941121sr@tcp(115.28.27.5:3306)/test")
+	if err != nil {
+		fmt.Printf("数据库连接出错：%s\n", err.Error())
+		return false
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Printf("ping不通数据库：%s\n", err.Error())
+		return false
+	}
+
+	result := db.QueryRow("select * from user where username = ? and password = ?", username, password)
+	if result == nil {
+		fmt.Printf("在查询的时候出错\n")
+		return false
+	}
+	var user User
+	err = result.Scan(&user.id, &user.username, &user.password)
+	if err != nil {
+		return false
+	}
+
+	fmt.Println(user)
+	return true
+}
+
+func VerifyEmail(email string, password string) bool {
+	db, err := sql.Open("mysql", "leslie:19941121sr@tcp(115.28.27.5:3306)/test")
+	if err != nil {
+		fmt.Printf("数据库连接出错：%s\n", err.Error())
+		return false
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Printf("ping不通数据库：%s\n", err.Error())
+		return false
+	}
+
+	result := db.QueryRow("select * from user where email = ? and password = ?", email, password)
+	if result == nil {
+		fmt.Printf("在查询的时候出错\n")
+		return false
+	}
+	var user User
+	err = result.Scan(&user.id, &user.username, &user.password)
+	if err != nil {
+		return false
+	}
+
+	fmt.Println(user)
+	return true
+}
